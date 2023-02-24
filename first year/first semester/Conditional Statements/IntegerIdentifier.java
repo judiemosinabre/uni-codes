70a42f1f828381b939e6d0ad21d77bbd70118a0f
J <116361492+j-archives@users.noreply.github.com>
2023-02-24 13:55:38 +0800

    positive, negative, or 0

---
+++ first year/first semester/Conditional Statements/IntegerIdentifier.java
@@ -0,0 +1,32 @@
+
+package conditionalstatements;
+
+import java.util.Scanner;
+public class IntegerIdentifier {
+
+   
+    public static void main(String[] args) {
+        //title
+        System.out.println("Integer Identifier");
+        
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("Enter the number: ");
+        int num = input.nextInt();
+        
+        if(num > 0) {
+            System.out.println(num + " is a Positive number");
+        }
+        
+        else if (num < 0) {
+            System.out.println(num + " is a Negative number");
+        }
+        else if (num == 0) {
+            System.out.println(num + " is a Number is zero");
+        }
+        else {
+            System.out.println("INVALID");
+        }
+    }
+    
+}
