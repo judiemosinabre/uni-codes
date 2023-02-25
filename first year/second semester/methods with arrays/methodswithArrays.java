fff5007a95bb1d16a3d8cb9385d36c173d1055d7
J <116361492+j-archives@users.noreply.github.com>
2023-02-25 12:07:52 +0800

    not yet sure about the output

---
+++ first year/second semester/methods with arrays/methodswithArrays.java
@@ -0,0 +1,32 @@
+package methods;
+
+import java.util.Scanner;
+public class methodswithArrays {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter size: ");
+        int size = scan.nextInt();
+        int [] array = new int[size];
+        
+        input(array, size);
+        output(array, size);
+    }
+    
+    public static void input (int [] array, int size) {
+        Scanner scan = new Scanner (System.in);
+        for (int i = 0; i < size; i++) {
+            System.out.print("Enter element: ");
+            array[i] = scan.nextInt();
+        }
+        
+        output(array, size);
+    }
+    
+    public static void output (int [] array, int size) {
+        System.out.print("Array elements: ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+    
+}
