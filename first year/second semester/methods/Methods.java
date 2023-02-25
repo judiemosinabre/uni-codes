2aba50f2593b8fdf0db97564b8856633afcbc127
J <116361492+j-archives@users.noreply.github.com>
2023-02-25 10:37:01 +0800

    odd, even, or zero (with methods)

---
+++ first year/second semester/methods/Methods.java
@@ -0,0 +1,27 @@
+package methods;
+
+import java.util.Scanner;
+public class Methods {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scan.nextInt();
+        
+        Checker(num);
+    }
+    
+    public static void Checker(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is even");
+        }
+        else if (num == 0) {
+            System.out.println(num + " is zero");
+        }
+        else {
+            System.out.println(num + " is odd");
+        }
+    }
+    
+}
